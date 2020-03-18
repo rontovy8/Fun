f950c462b9b07161f5e03901ead68b39ada75914
rontovy8 <62341555+rontovy8@users.noreply.github.com>
2020-03-18 16:16:18 +0200

    It's urgent yessss

---
+++ app/src/main/java/com/example/ronto/fun/HaimonLemon.java
@@ -0,0 +1,21 @@
+package com.example.ronto.fun;
+
+public class HaimonLemon
+{
+    private int a;
+
+    public HaimonLemon(int a)
+    {
+        this.a = a;
+    }
+
+    public int getA()
+    {
+        return a;
+    }
+
+    public void setA(int a)
+    {
+        this.a = a;
+    }
+}
