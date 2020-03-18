7f39e131acbf35f48d5d6641ff197c6f3215f81e
AvivLevinson2001 <62341707+AvivLevinson2001@users.noreply.github.com>
2020-03-18 17:18:43 +0200

    hi i added tamar class

---
+++ app/src/main/java/com/example/ronto/fun/tamar.java
@@ -0,0 +1,13 @@
+package com.example.ronto.fun;
+
+import android.util.Log;
+
+public class tamar extends Object {
+
+    int veganExper;
+    int numOfLegs;
+
+    public void say(){
+        Log.d("tamar", "ooof ani tamar");
+    }
+}
